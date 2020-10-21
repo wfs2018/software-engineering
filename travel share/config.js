10112c6143b19ba9dd99c7034c2a91f48ca1f519
wfs2018 <45965437+wfs2018@users.noreply.github.com>
2020-10-21 21:14:51 +0800

    Add files via upload
    first miniapp

---
+++ travel share/config.js
@@ -0,0 +1,17 @@
+// 全局配置
+
+module.exports = {
+
+
+    /** 输入框控件设置 **/
+    input: {
+        charWidth: 14,  // 单个字符的宽度，in rpx
+    },
+
+    /** 本地存储 **/
+
+    // TODO 数据通过API全部存储于服务端
+    // storage: {
+    //     // diaryListKey: 'bearDiaryList',
+    // }
+};
---
+++ travel share/demo/diaries.js
@@ -0,0 +1,175 @@
+var diaries = [
+  {
+    meta: {  // 内容元数据
+      cover: "#ffffff",
+      avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603260586631&di=e8fcb1cd93f6518b3de59277511dd62f&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2017-12-15%2F5a3372bd6de31.jpg",
+      title: "国庆之旅-2020年国庆节到达的北京，深圳，杭州，云南",
+      meta: "2020.10.1",
+      create_time: "2020.10.1 11:57:27",
+      nickName: "昊然",
+
+    },
+    list: [
+      {
+        type: "IMAGE",
+        content: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=215817509,3374955885&fm=26&gp=0.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "北京",
+        },
+        description: "国庆第一天，来天安门看升旗",
+        id: 2,
+        commentNum: 10,
+        likeNum: 200,
+      },
+      {
+          type: "IMAGE",
+          content: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603254787970&di=776f0101d41f918754712fc41b8dcf7c&imgtype=0&src=http%3A%2F%2Fpic.kekenet.com%2F2016%2F0511%2F52671462962184.jpg',
+          poi: {
+              longitude: 117.2,
+              latitude: 37.5,
+              name: "深圳市",
+          },
+          description: "深圳宝安国际机场",
+          id: 2,
+          commentNum: 1,
+          likeNum: 5,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603254951825&di=0794286f35161d404a8bb166fef2b86a&imgtype=0&src=http%3A%2F%2Fbreadtripimages.qiniudn.com%2Fphoto_2013_03_24_6e524dfb56705de33b8e23c1bfc47ab8.jpg%3FimageView%2F2%2Fw%2F960%2Fq%2F85',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "丽江",
+        },
+        description: "丽江三义机场",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603255016475&di=356987238f3c20d4adc3e56207d5575c&imgtype=0&src=http%3A%2F%2Fimg.wezhan.cn%2Fcontent%2Fsitefiles%2F1201%2Fimages%2F12432143_eb7c6f77938f0ebe9c696ca05426d81d.jpeg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "千岛湖",
+        },
+        description: "千岛湖，即新安江水库，位于浙江省杭州市淳安县境内，小部分连接杭州市建德市西北，是为建新安江水电站拦蓄新安江上游而成的人工湖，1955年始建，1960年建成。水库坝高105米，长462米；水库长约150千米，最宽处达10余千米；最深处达100余米，平均水深30.44米，在正常水位情况下，面积约580平方千米，蓄水量可达178亿立方米，在最高水位时拥有1078座大于0.25平方千米的陆桥岛屿，并以2平方千米以下的小岛为主，岛屿面积共409平方千米。 [1-3]  杭州千岛湖与加拿大渥太华西南200多千米的金斯顿千岛湖、湖北黄石阳新仙岛湖并称为世界三大千岛湖",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+    ]
+  },
+  {
+    meta: {  // 内容元数据
+      cover: "#ffffff",
+      avatar: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1806851781,2563165837&fm=26&gp=0.jpg",
+      title: "雅鲁藏布大峡谷-年轻的青藏高原何以形成如此奇丽、壮观的大峡谷？雅鲁藏布大峡谷形成的直接原因与该地区地壳3百万年来的快速抬升及深部地质作用有关。",
+      meta: "2020.10.13",
+      create_time: "2020.10.13 11:51:22",
+      nickName: "陈娜",
+    },
+    list: [
+      {
+        type: "IMAGE",
+        content: 'https://bkimg.cdn.bcebos.com/pic/728da9773912b31b5f19e88d8c18367adab4e119?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UxNTA=,g_7,xp_5,yp_5',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "雅鲁藏布大峡谷",
+        },
+        description: "雅鲁藏布大峡谷是地球上最深的峡谷。据国家测绘局公布的数据：这个大峡谷北起米林县派镇大渡卡村（海拔3000米），经排龙乡的雅鲁藏布江大拐弯，南到墨脱县巴昔卡村（海拔115米，在藏南地区靠近印度阿萨姆邦的地方），主体在墨脱县。从墨脱县甘登乡多卡村往上，一直到米林县派镇加拉村，是雅鲁藏布大峡谷无人区",
+        id: 1,
+        commentNum: 0,
+        likeNum: 0,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://bkimg.cdn.bcebos.com/pic/32fa828ba61ea8d34511bbee970a304e251f582d?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UxMTY=,g_7,xp_5,yp_5',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "雅鲁藏布大峡谷景区",
+        },
+        description: "",
+        id: 1,
+        commentNum: 0,
+        likeNum: 0,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://bkimg.cdn.bcebos.com/pic/4034970a304e251f28c42b12a786c9177f3e532d?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U5Mg==,g_7,xp_5,yp_5',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "大峡谷景区",
+        },
+        description: "年轻的青藏高原何以形成如此奇丽、壮观的大峡谷？雅鲁藏布大峡谷形成的直接原因与该地区地壳3百万年来的快速抬升及深部地质作用有关。15万年以来，大峡谷地区的抬升速度达到30毫米/年，是世界抬升最快的地区之一。最新地质考察获得的证据表明，大峡谷形成的根本原因是该地区存在着软流圈地幔上涌体。雅鲁藏布大峡谷形成的地质特征和美国科罗拉多大峡谷基本相似。雅鲁藏布大峡谷地区地幔上涌体可能是大峡谷水汽通道形成的一个重要因素，也可能是以该地区为中心的藏东南成为所谓“气候启动区”的原因，还可能是该地区生物纬向分布北移3-5度的重要原因。以地幔上涌体为特征的岩石圈物质和结构调整对地球外圈层长尺度制约作用在大峡谷地区有十分明显的表现，因此这里是地球系统中层圈耦合作用研究最理想的野外实验室。",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+    ]
+  },
+  {
+    meta: {  // 内容元数据
+      cover: "#ffffff",
+      avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603259980198&di=4117cac65c6a7ca036a6ac54ee3c61c9&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20200111%2Fd20c119082da4ab88c0aca36f84ecd3b.jpeg",
+      title: "情侣路是珠海的名片，长28公里，沿途热门景点有九洲港、海滨泳场、珠海渔女、香炉湾等。在路上散步可瞭望大海，吹着海风，遥望对岸的澳门，享受悠闲惬意的时光。体力好还可以骑行全程，在拱北附近或者名亭公园（野狸岛）门口都提供专用的自行车租借服务。",
+      meta: "2020.10.15",
+      create_time: "2020.10.15 11:51:22",
+      nickName: "于甜",
+    },
+  },
+  {
+    meta: {  // 内容元数据
+      cover: "#ffffff",
+      avatar: "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2319848669,1790212985&fm=26&gp=0.jpg",
+      title: "横琴蚝生活在横琴岛二得名，横琴岛四面环海，在咸淡水的交界处，由于其温度适宜，水质干净，是生长在这里的横琴蚝肥硕鲜美，味道清甜",
+      meta: "2020.10.16",
+      create_time: "2020.10.16 11:51:22",
+      nickName: "莹莹",
+    },
+  },
+  {
+    meta: {  // 内容元数据
+      cover: "#ffffff",
+      avatar: "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3649482971,3541580223&fm=26&gp=0.jpg",
+      title: "·位于大香格里拉风景区的中心区域，是世界自然遗产“三江并流”的重要组成部分。以碧塔海、属都湖和弥里塘亚高山牧场为主要组成部分，是香格里拉旅游的主要景点之一。拥有地质地貌、湖泊湿地、森林草甸、河谷溪流、珍稀动植物等，原始生态环境保存完好。普达措是摄影爱好者的天堂，春秋两季风光尤其秀美，是来普达措游览或摄影的最佳时节",
+      meta: "2020。10.17",
+      create_time: "2020.10.17 11:51:22",
+      nickName: "晨乐",
+    },
+  },
+  {
+    meta: {  // 内容元数据
+      cover: "#ffffff",
+      avatar: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3850473369,3170723670&fm=26&gp=0.jpg",
+      title: "澳大利亚10日游 悉尼 → 凯恩斯 → 库兰达 → 凯恩斯 → 墨尔本 → 大洋路 → 坎贝尔港 → 大洋路 → 墨尔本",
+      meta: "2020.10.18",
+      create_time: "2020.10.18 11:51:22",
+      nickName: "宇航",
+    },
+  },
+  {
+    meta: {  // 内容元数据
+      cover: "#ffffff",
+      avatar: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=126207350,2649195067&fm=26&gp=0.jpg",
+      title: "神仙海岛的三款热门玩法，去普吉一定不能错过",
+      meta: "2020.10.19",
+      create_time: "2020.10.19 11:51:22",
+      nickName: "刘佳",
+    },
+  }
+
+
+ 
+]
+
+module.exports = {
+  diaries: diaries,
+}
